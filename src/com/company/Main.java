aab14e70e5753c87181e477d5e181d17a73ae882
Vinay Nagalgaonkar <vinayrn@amazon.com>
2020-09-29 17:19:24 +0530

    Changed commit msg of c2 with 'git commit --amend'

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,11 @@
 package com.company;
 
+import com.company.someClasses.Apple;
+
 public class Main {
 
     public static void main(String[] args) {
-	    // write your code here
+        Apple apple = new Apple();
+        apple.printFunction1();
     }
 }
