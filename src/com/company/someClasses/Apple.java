acca22730a6da9868d88378b633c6c3edb7f298c
Vinay Nagalgaonkar <vinayrn@amazon.com>
2020-09-29 18:53:27 +0530

    Added a function in Apple.java for branch br1FromMaster

--- src/com/company/someClasses/Apple.java
+++ src/com/company/someClasses/Apple.java
@@ -4,4 +4,8 @@ public class Apple {
     public void printFunction1(){
         System.out.println("This is printFunction 1");
     }
+
+    public void printFunctionForBr1FromMasterBranch(){
+        System.out.println("This is printFunction is to signify some code change in 'br1FromMaster' branch");
+    }
 }
